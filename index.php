63b4e885f2575f6ed7383a4b42fa38d2535f24a0
Bruno <brubarratella@hotmail.com>
2023-03-23 14:57:15 -0300

    Primeiro commit: desevolvendo versão mobile do site

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cine Belluzzo</title>
+    <link rel="shortcut icon" href="./img/3418886.png">
+    <link rel="stylesheet" href="./css/estilo.css">
+</head>
+
+<body>
+    <header>
+        <div class="headerContainer">
+            <div class="headerContent">
+                <img src="./img/LogoCine.png" alt="Logo Cinebelluzzo" class="logo">
+
+                <input type="checkbox" id="menu_hamburguer" class="checkMenu">
+                <label for="menu_hamburguer">
+                    <span class="menu"></span>
+                </label>
+                <ul class="navbar">
+                    <li>Home</li>
+                    <li>Em cartaz</li>
+                    <li>Bomboniere</li>
+                    <button class="contactBtn">Contato</button>
+                </ul>
+                
+
+            </div>
+            
+        </div>
+        
+    </header>
+</body>
+
+</html>
